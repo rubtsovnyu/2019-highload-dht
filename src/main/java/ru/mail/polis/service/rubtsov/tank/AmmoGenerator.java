97f3fd27286b86c70945a39212cc05a4d5582ef8
keet <nikolay1337@icloud.com>
2019-11-22 22:23:00 +0300

    rename newLine to NEW_LINE

--- src/main/java/ru/mail/polis/service/rubtsov/tank/AmmoGenerator.java
+++ src/main/java/ru/mail/polis/service/rubtsov/tank/AmmoGenerator.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class AmmoGenerator {
     private static final int VALUE_LENGTH = 256;
-    private static final String newLine = "\r\n";
+    private static final String NEW_LINE = "\r\n";
 
     public static void main(final String[] args) throws IOException {
         if (args.length < 2 || args.length > 3) {
@@ -108,14 +108,14 @@ public class AmmoGenerator {
         final ByteArrayOutputStream request = new ByteArrayOutputStream();
         try (Writer writer = new OutputStreamWriter(request, StandardCharsets.US_ASCII)) {
             writer.write("PUT /v0/entity?id=" + key + " HTTP/1.1\r\n");
-            writer.write("Content-Length: " + value.length + newLine);
-            writer.write(newLine);
+            writer.write("Content-Length: " + value.length + NEW_LINE);
+            writer.write(NEW_LINE);
         }
         request.write(value);
         System.out.write(Integer.toString(request.size()).getBytes(StandardCharsets.US_ASCII));
         System.out.write(" PUT\n".getBytes(StandardCharsets.US_ASCII));
         request.writeTo(System.out);
-        System.out.write(newLine.getBytes(StandardCharsets.US_ASCII));
+        System.out.write(NEW_LINE.getBytes(StandardCharsets.US_ASCII));
     }
 
     private static void get(final long keyLong) throws IOException {
@@ -123,12 +123,12 @@ public class AmmoGenerator {
         final ByteArrayOutputStream request = new ByteArrayOutputStream();
         try (Writer writer = new OutputStreamWriter(request, StandardCharsets.US_ASCII)) {
             writer.write("GET /v0/entity?id=" + key + " HTTP/1.1\r\n");
-            writer.write(newLine);
+            writer.write(NEW_LINE);
         }
         System.out.write(Integer.toString(request.size()).getBytes(StandardCharsets.US_ASCII));
         System.out.write(" GET\n".getBytes(StandardCharsets.US_ASCII));
         request.writeTo(System.out);
-        System.out.write(newLine.getBytes(StandardCharsets.US_ASCII));
+        System.out.write(NEW_LINE.getBytes(StandardCharsets.US_ASCII));
     }
 
     private static String keyFromLong(final long key) {
